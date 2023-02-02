f93dbcdf43d35b6e3016f833c02ff91af7842b35
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-02 20:37:09 +0530

    navbar completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,6 +13,21 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="Search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] loading-[30px] text-white">
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="Menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
+    </div>
   </motion.nav>
 );
 
