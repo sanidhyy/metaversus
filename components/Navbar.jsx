b3307c4028882399a734440b1d292615c0a38bf2
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-02 20:28:29 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+  </motion.nav>
+);
+
+export default Navbar;
