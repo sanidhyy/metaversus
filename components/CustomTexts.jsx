7e50987dccca371733cb253419e39ab93fe1e485
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-04 19:32:02 +0530

    explore section completed

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -17,4 +17,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+);
