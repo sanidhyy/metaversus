74a55339caca9f84b6389e4265a1a6fa7f3de2b0
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-04 19:57:11 +0530

    typo fixed in title

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -31,7 +31,7 @@ const GetStarted = () => (
         variants={fadeIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title="| How Metaversus Works" />
+        <TypingText title="| Get Started" />
         <TitleText title={<>Get Started with just a few clicks</>} />
         <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
           {startingFeatures.map((feature, i) => (
