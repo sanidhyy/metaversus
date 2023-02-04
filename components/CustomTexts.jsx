17df0eadb3fa4c0f735a2222c0cf8aad7c8e6654
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-04 18:52:17 +0530

    about page created

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,20 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, i) => (
+      <motion.span variants={textVariant2} key={i}>
+        {letter === " " ? "\u00A0" : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
