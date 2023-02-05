6be5b268188c04bcef84509d2ca648a9c3869fb3
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-02-05 20:41:28 +0530

    eslint rules updated

--- components/index.js
+++ components/index.js
@@ -1,10 +1,10 @@
-import Navbar from './Navbar';
-import { TypingText, TitleText } from './CustomTexts';
-import ExploreCard from './ExploreCard';
-import StartSteps from './StartSteps';
-import NewFeatures from './NewFeatures';
-import InsightCard from './InsightCard';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import { TypingText, TitleText } from "./CustomTexts";
+import ExploreCard from "./ExploreCard";
+import StartSteps from "./StartSteps";
+import NewFeatures from "./NewFeatures";
+import InsightCard from "./InsightCard";
+import Footer from "./Footer";
 
 export {
   Navbar,
